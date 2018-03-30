131071adb49b80c15a8f5553a155c000a9d873c6
Maddie Wilkinson <mw350@duke.edu>
2018-03-30 00:11:57 -0400

    finished Authoring API

--- doc/api/gameAuthoring/Behavior.java
+++ doc/api/gameAuthoring/Behavior.java
@@ -1,11 +1,12 @@
-//has 3 things: a list of properties, which any behavior of that type will have
-//for instance: movement will have x velocity, y velocity
-//within this, you'll have actions that the property uses 
-//if someone says a gameobject has movement behavior, it'll automatically add an xvelocity and yvelocity,
-//as well as a list of actions associated with that behavior that the user can choose
+
 public interface Behavior {
 	
 	public void setProperties(List<Property> properties) //adds properties 
 	
+	public List<Property> getProperties() //gets properties 
+	
+	public String getName(); //returns the name of the property
+	
+	public void setName(String name); //sets or resets the name of the property
 	
 }

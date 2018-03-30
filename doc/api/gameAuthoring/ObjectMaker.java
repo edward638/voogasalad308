c62df92304024430fd78c77b9011bf587f6ff897
Summer Smith <sas118@duke.edu>
2018-03-29 20:56:50 -0400

    authoring api comments

--- doc/api/gameAuthoring/ObjectMaker.java
+++ doc/api/gameAuthoring/ObjectMaker.java
@@ -1,10 +1,10 @@
 
 public interface ObjectManager {
 
-	public GameObject makeGameObject();
+	public GameObject makeGameObject(); //makes a game object with no properties
 	
-	public void deleteGameObject(GameObject gameObject);
+	public void deleteGameObject(GameObject gameObject); //deletes a game object
 	
-	public List<> getGameObjects();
+	public List<> getGameObjects(); //returns the list of all current game objects
 
 }
--- doc/api/gameAuthoring/Property.java
+++ doc/api/gameAuthoring/Property.java
@@ -1,12 +1,12 @@
 
 public interface Property {
 	
-	public void setParameters(List<> paramList); //
+	public void setParameters(List<> paramList); //sets the parameters of the property
 	
-	public List<> getParameters();
+	public List<> getParameters(); //returns the parameters of the property
 	
-	public String getName();
+	public String getName(); //returns the name of the property
 	
-	public void setName(String name);
+	public void setName(String name); //sets or resets the name of the property
 	
 }

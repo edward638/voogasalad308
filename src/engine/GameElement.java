1d04977416232cb48118653fd2e81756f7d269b6
Trishul Nagenalli <tn74@duke.edu>
2018-04-01 12:17:27 -0400

    Added getElementIdentifier for GameElement

--- src/engine/GameElement.java
+++ src/engine/GameElement.java
@@ -1,6 +1,8 @@
 package engine;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import engine.behaviors.BasicGameElement;
 import engine.behaviors.Behavior;
@@ -20,9 +22,27 @@ public class GameElement {
 		behaviors.add(behave);
 	}
 	
+	public Behavior getBehavior (Class<?> behavior_type) {
+		try {
+			return behaviors.stream().filter(behavior -> behavior.getClass() == behavior_type).collect(Collectors.toList()).get(0));
+		catch (NullPointerException n) {
+			throw new IllegalArgumentException(behavior_type + " does not exist for GameElement" + getIdentifier());
+		}
+		}
 	/*
 	 * Adds the ability for this game element to respond to an ElementEvent in a certain way
 	 */
+	
+	/*
+	 * Defines the method we will use to identify this game element. Should be done according the 
+	 * BasicGameElement behavior since every element in the game will implement that
+	 */
+	public String getIdentifier() {
+		BasicGameElement el = (BasicGameElement) behaviors.stream()
+				.filter(b -> b.getClass() == new BasicGameElement(this, " ", 100.0, 100.0).getClass())
+				.collect(Collectors.toList()).get(0);
+		return el.getName();
+	}
 	public void addEventReaction() {
 		
 	}

e83324dd8f288e4bf8a0fbd7d7795e283cb64762
Edward Zhuang <edward.zhuang@duke.edu>
2018-04-03 16:22:44 -0400

    added methods to serializer

--- src/Data/Serializer.java
+++ src/Data/Serializer.java
@@ -1,27 +1,57 @@
 package Data;
 
+import authoring.GameObject;
+import authoring.GameScene;
 import com.thoughtworks.xstream.XStream;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Serializer {
 
+    private static final String LEVEL = "level";
+    private static final String GAMEOBJECT = "gameobject";
     private XStream xstream;
 
     public Serializer(){
         xstream = new XStream();
-        System.out.println("hi");
     }
 
     /**
      * Converts game to XML. Used by game authoring environment.
      *
      * @param fileName    desired name of XML game file to be created
-     * @param gameObjects list of GameObjects which the game is comprised of
+     * @param objectMap  map provided from authoring environment which maps GameScene to a list of GameObjects contained on that scene
      */
-    void gameAuthorToXML(String fileName, TestObject gameObjects) {
+    void gameAuthorToXML(String fileName, Map<GameScene, List<GameObject>> objectMap) throws IOException {
+        String topLevelGameDestination = "./src/Data/XMLFiles/" + fileName;
+        new File(topLevelGameDestination).mkdirs();
+
+        int x = 1;
+        for (GameScene key: objectMap.keySet()){
+            String levelGameDestination = topLevelGameDestination + "/" + LEVEL + x;
+            for (GameObject gameObject: objectMap.get(key)){
+                String xmlString = xstream.toXML(gameObject);
+                stringToDom(xmlString, levelGameDestination + "/" + gameObject.getName() + ".xml");
+            }
+            x++;
+        }
+    }
+
+    // taken from https://stackoverflow.com/questions/17853541/java-how-to-convert-a-xml-string-into-an-xml-file
+
+    private static void stringToDom(String xmlSource, String fileName)
+            throws IOException {
+        java.io.FileWriter fw = new java.io.FileWriter(fileName);
+        fw.write(xmlSource);
+        fw.close();
+    }
+
+    void testGameAuthorToXML(String fileName, TestObject gameObjects) throws IOException {
         String test = xstream.toXML(gameObjects);
+        stringToDom(test, "./src/Data/XMLFiles/hello/test.xml");
         System.out.println(test);
     }
 
@@ -50,9 +80,14 @@ public class Serializer {
           Serializer serializer = new Serializer();
 
           String[] stringList = {"hello", "goodbye", "yo", "hello"};
-          TestObject testObject = new TestObject(3, "biiiiich", stringList);
+          TestObject testObject = new TestObject(3, "biiiiich", stringList, "Goomba.png");
+          try {
+              serializer.testGameAuthorToXML("hello", testObject);
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
 
-          serializer.gameAuthorToXML("hello", testObject);
+//
 
 
       }
--- src/authoring/ObjectManager.java
+++ src/authoring/ObjectManager.java
@@ -14,32 +14,32 @@ public class ObjectManager {
 	}
 	
 	//makes a game object with no properties
-	public GameObject makeObjectTemplate() {
-		
-	}
-	
-	public void placeGameObject(GameScene currScene, GameObject toPlace) {
-		
-	}
-
-	//makes a game object with the given property
-	public GameObject makeGameObject(Property prop) { 
-		gameObject = new GameObject();
-		gameObject.addProperty(prop);
-	}
-	
-	//deletes a game object
-	public void deleteGameObject(GameObject gameObject) {
-		
-	}
-
-	//returns the list of all game objects currently placed in the game
-	public List<GameObject> getPlacedObjects() {
-		
-	}
-
-	//returns the list of all game object "templates" available to use
-	public List<GameObject> getObjectTemplates() {
-		
-	}
+//	public GameObject makeObjectTemplate() {
+//
+//	}
+//
+//	public void placeGameObject(GameScene currScene, GameObject toPlace) {
+//
+//	}
+//
+//	//makes a game object with the given property
+//	public GameObject makeGameObject(Property prop) {
+//		GameObject gameObject = new GameObject();
+//		gameObject.addProperty(prop);
+//	}
+//
+//	//deletes a game object
+//	public void deleteGameObject(GameObject gameObject) {
+//
+//	}
+//
+//	//returns the list of all game objects currently placed in the game
+//	public List<GameObject> getPlacedObjects() {
+//
+//	}
+//
+//	//returns the list of all game object "templates" available to use
+//	public List<GameObject> getObjectTemplates() {
+//
+//	}
 }

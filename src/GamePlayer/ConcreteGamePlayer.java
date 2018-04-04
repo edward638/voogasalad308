5b9db6dccdaf81d14fcb4986c2e05585f84d006d
Jeffrey <jql3@duke.edu>
2018-04-04 18:08:21 -0400

    APIS moved

---
+++ src/GamePlayer/ConcreteGamePlayer.java
@@ -0,0 +1,23 @@
+package GamePlayer;
+
+public class ConcreteGamePlayer implements GamePlayer{
+
+	@Override
+	public void initializePlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playGame(String game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveGame() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}

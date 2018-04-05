01e760cf8650c4a0c6cf786e47c754addc528c21
August Ning <august.ning@duke.edu>
2018-04-04 22:02:10 -0400

    fixed inline commit conflicts

--- src/display/SplashScreen.java
+++ src/display/SplashScreen.java
@@ -2,19 +2,13 @@ package display;
 
 /**
  * @author August Ning
-<<<<<<< HEAD
  * class that houses the SplashScreen
-=======
  *
->>>>>>> fe0b352e4901c21402382947c3c100ad2354ba6c
  */
 public class SplashScreen {
 
 	public SplashScreen() {
-<<<<<<< HEAD
-=======
 		// TODO Auto-generated constructor stub
->>>>>>> fe0b352e4901c21402382947c3c100ad2354ba6c
 	}
 
 }

364c6552ac769c0cc3010a20acb5dcec91f928e5
Trishul Nagenalli <tn74@duke.edu>
2018-04-04 21:18:17 -0400

    Pre meeting 9 pm wed apr 4

--- src/engine/tests/GameElementTesting.java
+++ src/engine/tests/GameElementTesting.java
@@ -1,21 +1,44 @@
 package engine.tests;
 
-import java.util.Map;
+import java.util.Arrays;
 
 import engine.GameElement;
+import engine.behaviors.BasicGameElement;
 import engine.behaviors.Behavior;
 import engine.behaviors.Movable;
+import engine.eventresponses.BasicTimeResponse;
+import engine.events.TimeEvent;
 
 public class GameElementTesting {
 	
 	public static void main(String[] args) {
-		addBehaviorTest();
+		testEventHandling();
 	}
+	
+	
 	public static void addBehaviorTest() {
 		GameElement ge = new GameElement();
+		ge.addBehavior(new BasicGameElement(ge, "Mario", 100.0, 100.0));
 		System.out.println(ge.reportProperties());
 		Behavior b = new Movable(ge);
 		ge.addBehavior(b);
+		System.out.println(ge.reportProperties());
+	}
+	
+	public static void testEventHandling() {
+		GameElement ge = new GameElement();
+		ge.addBehavior(new BasicGameElement(ge, "Mario", 100.0, 100.0));
+		System.out.println(ge.reportProperties());
+		
+		ge.addBehavior(new Movable(ge));
+		
+		ge.addEventResponse(new BasicTimeResponse());
+		System.out.println(ge.reportProperties());
+		
+		ge.processEvent(new TimeEvent(5.0));
+		System.out.println(ge.reportProperties());
+		
+		ge.processEvent(new TimeEvent(15.0));
 		System.out.println(ge.reportProperties());
 	}
 }

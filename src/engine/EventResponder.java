f4aed2319f931e4ea388a84fc33c78b41a2d9bb7
Trishul Nagenalli <tn74@duke.edu>
2018-04-09 18:01:09 -0400

    Created EventResponder Class and added methods

---
+++ src/engine/EventResponder.java
@@ -0,0 +1,29 @@
+package engine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import engine.actions.Action;
+import engine.events.elementevents.ElementEvent;
+
+public class EventResponder {
+	private Map <ElementEvent, Action> responses;
+	private GameElement parent;
+	
+	public EventResponder(GameElement ge) {
+		responses = new HashMap<>();
+		parent = ge;
+	}
+	
+	public void addResponse(ElementEvent e, Action a) {
+		responses.put(e,  a);
+	}
+	
+	public void respondTo(ElementEvent other) {
+		for (ElementEvent e: responses.keySet()) {
+			if (e.matchesEvent(other)) {
+				responses.get(e).act(other, parent);
+			}
+		}
+	}
+}

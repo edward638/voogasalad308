23582a2b6d5e0e53164ae6bfc51336a8f2a30a42
Calvin Ma <cm298@duke.edu>
2018-04-08 20:19:32 -0400

    merging

--- src/GamePlayer/ConcreteHUD.java
+++ src/GamePlayer/ConcreteHUD.java
@@ -23,8 +23,9 @@ public class ConcreteHUD extends Pane implements HUD{
 	
 
 	public ConcreteHUD(String name) {
-		setupPane();
 		gameName = name;
+		setupPane();
+
 	}
 
 
@@ -46,26 +47,37 @@ public class ConcreteHUD extends Pane implements HUD{
 	}
 	
 	private void setupHUDText() {
+		System.out.println(gameName);
 		Label nameText = new Label(gameName);
+		nameText.setTextFill(Color.BLACK);
 		nameText.setLayoutX(xLabel);
-		xLabel = xLabel + 150;
-		nameText.setLayoutY(yLabel);
+		nameText.setLayoutY(yLabel - 25);
 		nameText.setFont(new Font(20));
 		this.getChildren().add(nameText);
 		Label scoreText = new Label("Score: " + currentScore);
+		scoreText.setTextFill(Color.WHITE);
+
 		scoreText.setLayoutX(xLabel);
 		xLabel = xLabel + 150;
 		scoreText.setLayoutY(yLabel);
 		scoreText.setFont(new Font(20));
 		this.getChildren().add(scoreText);
 		Label livesText = new Label("Lives: " + livesRemaining);
+		livesText.setTextFill(Color.WHITE);
+
 		livesText.setLayoutX(xLabel);
 		xLabel = xLabel + 150;
 		livesText.setLayoutY(yLabel);
 		livesText.setFont(new Font(20));
 		this.getChildren().add(livesText);
 		for (String label : otherInfo.keySet()) {
+			if ((xLabel + 100) > 700) {
+				xLabel = 0;
+				yLabel = yLabel + 25;
+			}
 			Label otherText = new Label(label + ": " + otherInfo.get(label));
+			otherText.setTextFill(Color.WHITE);
+
 			otherText.setLayoutX(xLabel);
 			xLabel = xLabel + 150;
 			otherText.setLayoutY(yLabel);
@@ -80,6 +92,12 @@ public class ConcreteHUD extends Pane implements HUD{
 		HashMap other = new HashMap<String, Integer>();
 		other.put("Kills", 12);
 		other.put("Ammo", 8);
+		other.put("Ammo1", 8);
+		other.put("Ammo2", 8);
+		other.put("Ammo3", 8);
+		other.put("Ammo4", 8);
+		other.put("Ammo5", 8);
+
 		other.put("Current Player", "Jeffrey");
 		updateInfo(758, 7, other);
 	}

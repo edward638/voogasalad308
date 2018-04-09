9975bdcf7d896cba479368cfb6b0d8f9bb9a7b94
Trishul Nagenalli <tn74@duke.edu>
2018-04-09 18:06:36 -0400

    Created TimeMovable Action

---
+++ src/engine/actions/TimeMovable.java
@@ -0,0 +1,16 @@
+package engine.actions;
+
+import engine.GameElement;
+import engine.behaviors.Movable;
+import engine.events.elementevents.ElementEvent;
+import engine.events.elementevents.TimeEvent;
+
+public class TimeMovable implements Action{
+	
+	@Override
+	public void act(ElementEvent event, GameElement element) {
+		TimeEvent te = (TimeEvent) event;
+		Movable b = (Movable) element.getBehavior(Movable.class);
+		b.move(te.getTime());
+	}
+}

e414525dbe0d9d00c741010dd69b2eb0fd4b57b3
Trishul Nagenalli <tn74@duke.edu>
2018-04-09 19:22:11 -0400

    Rectangle shape implemented with Mandatory Behavior

--- src/engine/behaviors/MandatoryBehavior.java
+++ src/engine/behaviors/MandatoryBehavior.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import engine.GameElement;
+import engine.behaviors.shapes.RectangleShape;
+import engine.behaviors.shapes.ShapeDefinition;
 import javafx.scene.shape.Shape;
 
 public class MandatoryBehavior extends Behavior{
@@ -11,12 +13,18 @@ public class MandatoryBehavior extends Behavior{
 	private Double xPos;
 	private Double yPos;
 	private String elementName;
+	private ShapeDefinition shapeDef;
 	
-	public MandatoryBehavior(GameElement ge, String name, Double startX, Double startY) {
+	public MandatoryBehavior(GameElement ge, String name, Double startX, Double startY, ShapeDefinition shp) {
 		super(ge);
 		xPos = startX;
 		yPos = startY;
 		elementName = name;
+		shapeDef = shp;
+	}
+	
+	public MandatoryBehavior(GameElement ge, String name, Double startX, Double startY) {
+		this(ge, name, startX, startY, new RectangleShape(100.0, 100.0));
 	}
 	
 	public void setPosition(double x, double y) {
@@ -39,7 +47,7 @@ public class MandatoryBehavior extends Behavior{
 	}
 	
 	public Shape getShape() {
-		
+		return shapeDef.getShape(this);
 	}
 	public String getName() {
 		return elementName;

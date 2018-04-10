683c8bc91c4c2a1f63d88093bab40b82b43c589f
Trishul Nagenalli <tn74@duke.edu>
2018-04-10 14:04:28 -0400

    Changed movable to use xVel and yVel

--- src/engine/behaviors/Movable.java
+++ src/engine/behaviors/Movable.java
@@ -11,15 +11,23 @@ public class Movable extends Behavior{
 	public static final int X_LIST_POS = 0; // Position of X in direction 
 	public static final int Y_LIST_POS = 1; // Position of Y in direction
 	
-	private Double velocity;
-	private List<Double> direction;
+//	private Double velocity;
+//	private List<Double> direction;
 	private Double xVel;
 	private Double yVel;
 	
 	public Movable(GameElement ge, Double vel, List<Double> dir) {
 		super(ge);
+		xVel = 0.0;
+		yVel = 1.0;
 		setVelocity(vel);
 		setDirection(dir);
+	}
+	
+	public Movable (GameElement ge, Double xv, Double yv) {
+		super(ge);
+		xVel = xv; 
+		yVel = yv;
 	}
 	
 	public Movable(GameElement ge) {
@@ -39,80 +47,41 @@ public class Movable extends Behavior{
 	 * is valid and normalizes it so move method works correctly
 	 */
 	public void setDirection(List<Double> dir) {
-		if (isValidDirection(dir)) {
-			Double total = dir.stream().reduce(0.0, (a, b) -> a + b);
-			List<Double> normalized = dir.stream().map(n -> n/total).collect(Collectors.toList());
-			direction = normalized;
-		} else {
-			throw new IllegalArgumentException("Illegal Direction for " + getParent().getIdentifier() + ": " + dir);
-		}
+		Double v = getVelocity();
+		setXVelocity(v * dir.get(0));
+		setYVelocity(v * dir.get(1));
 	}
 	
 	/*
 	 * Sets the velocity according to what the other classes want. Checks if velocity is valid for this object 
 	 */
 	public void setVelocity (Double v) {
-		if (isValidVelocity(v)) {
-			velocity = v;
-		} else {
-			throw new IllegalArgumentException("Illegal Velocity for " + getParent().getIdentifier() + ": " + v);
-		}
+		xVel = getDirection().get(0) * v;
+		yVel = getDirection().get(1) * v;
 	}
 	
 	public void setXVelocity (Double xv) {
-		if (isValidVelocity(xv)) {
-			Double newxv = getXVelocity() + xv;
-			Double yv = getYVelocity();
-			setDirection(Arrays.asList(newxv, yv));
-			velocity = Math.sqrt(Math.pow(newxv, 2.0) + Math.pow(yv, 2.0));
-//			Double tempvelocity = getYVelocity() + xv;
-//			setDirection(Arrays.asList(xv/tempvelocity, getYVelocity()/xv));
-//			velocity = tempvelocity;
-		} else {
-			throw new IllegalArgumentException("Illegal Velocity for " + getParent().getIdentifier() + ": " + xv);
-		}
+		xVel = xv;
 	}
 	
 	public void setYVelocity (Double yv) {
-		if (isValidVelocity(yv)) {
-			Double xv = getXVelocity();
-			Double newyv = getYVelocity() + yv;
-			setDirection(Arrays.asList(xv, newyv));
-			velocity = Math.sqrt(Math.pow(xv, 2.0) + Math.pow(newyv, 2.0));
-//			Double tempvelocity = getXVelocity() + yv;
-//			setDirection(Arrays.asList(yv/tempvelocity, getXVelocity()/yv));
-//			velocity = tempvelocity;
-		} else {
-			throw new IllegalArgumentException("Illegal Velocity for " + getParent().getIdentifier() + ": " + yv);
-		}
+		yVel = yv;
 	}
 	
 	public double getXVelocity () {
-		return velocity * direction.get(X_LIST_POS);
+		return xVel;
 	}
 	
 	public double getYVelocity () {
-		return velocity * direction.get(Y_LIST_POS);
+		return yVel;
 	}
 	
 	public Double getVelocity () {
-		return velocity;
+		return Math.sqrt(Math.pow(xVel, 2) + Math.pow(yVel, 2));
 	}
 	
 	public List<Double> getDirection() {
-		return direction;
-	}
-	
-	private boolean isValidDirection(List<Double> d) {
-		if (d.size() > 2 || 
-				d.stream().reduce(0.0, (a, b) -> Math.abs(a) + Math.abs(b)) == 0) {
-			return false;
-		}
-		return true;
-	}
-	
-	private boolean isValidVelocity(Double v) {
-		return true;
+		return Arrays.asList(xVel / getVelocity(), yVel /getVelocity());
 	}
 	
 }

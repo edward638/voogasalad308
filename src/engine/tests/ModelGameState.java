0e882997f298709a0b35c7cc6c0c8687075bf8cb
Trishul Nagenalli <tn74@duke.edu>
2018-04-10 09:37:50 -0400

    Creating sample GameState maker

---
+++ src/engine/tests/ModelGameState.java
@@ -0,0 +1,70 @@
+package engine.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import engine.GameElement;
+import engine.GameState;
+import engine.actions.TimeGravity;
+import engine.actions.TimeMovable;
+import engine.behaviors.Gravity;
+import engine.behaviors.MandatoryBehavior;
+import engine.behaviors.Movable;
+import engine.behaviors.shapes.RectangleShape;
+import engine.events.elementevents.CollisionEvent;
+import engine.events.elementevents.KeyInputEvent;
+import engine.events.elementevents.TimeEvent;
+import javafx.scene.input.KeyCode;
+
+public class ModelGameState {
+	private GameState state;
+	
+	public ModelGameState() {
+	}
+	
+	public GameState getState() {
+		state = new GameState();
+		state.addGameElement(getMario());
+		return state;
+	}
+	
+	public GameElement getMario() {
+		GameElement mario = new GameElement();
+		mario.addBehavior(new MandatoryBehavior(mario, "Mario", 10.0, 10.0, new RectangleShape(20.0, 40.0)));
+		List<Double> direction = new ArrayList<>(); direction.add(1.0); direction.add(0.0);
+;		mario.addBehavior(new Movable(mario, 0.0, direction));
+		mario.addBehavior(new Gravity(mario));
+		
+		//Adding Time Responses
+		mario.addEventResponse(new TimeEvent(0.0), new TimeMovable());
+		mario.addEventResponse(new TimeEvent(0.0), new TimeGravity());
+		// Response to up arrow key is to jump
+		mario.addEventResponse(new KeyInputEvent(KeyCode.UP), (event, element) -> {
+			Movable mov = (Movable) element.getBehavior(Movable.class);
+			mov.setYVelocity(mov.getYVelocity() + 30);
+		});
+		
+		// Response to Right arrow key is to move right
+		mario.addEventResponse(new KeyInputEvent(KeyCode.RIGHT), (event, element) -> {
+			Movable mov = (Movable) element.getBehavior(Movable.class);
+			mov.setXVelocity(10.0);
+		});
+		
+		// Response to Left arrow key is to move left
+		mario.addEventResponse(new KeyInputEvent(KeyCode.RIGHT), (event, element) -> {
+			Movable mov = (Movable) element.getBehavior(Movable.class);
+			mov.setXVelocity(-10.0);
+		});
+		return mario;
+	}
+	
+	public GameElement getBlock(Double xpos, Double ypos) {
+		GameElement block = new GameElement();
+		block.addBehavior(new MandatoryBehavior(block, "Block", xpos, ypos, new RectangleShape(20.0, 20.0)));
+		block.addEventResponse(new CollisionEvent(getMario(), block), (event, element) -> {
+			CollisionEvent ce = (CollisionEvent) event;
+			event.
+		});
+		return block;
+	}
+}

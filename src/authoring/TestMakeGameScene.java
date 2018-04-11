0f773c2bee5819d673946a0cb341b17f4a2de3e9
Edward Zhuang <edward.zhuang@duke.edu>
2018-04-10 22:57:50 -0400

    updated gameviewwindow

--- src/authoring/TestMakeGameScene.java
+++ src/authoring/TestMakeGameScene.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import data.*;
+import engine.tests.ModelGameState;
 import javafx.scene.image.Image;
 
 public class TestMakeGameScene {
@@ -14,31 +15,36 @@ public class TestMakeGameScene {
 	
 	public static void main(String[] args) throws IOException {
 		
-		GameScene gameScene = new GameScene("Level 1");
-		BehaviorFactory bf = new BehaviorFactory();
-		GameObject go1 = new GameObject(bf.makeBehavior("BasicGameElement"));
-		go1.getBehavior("BasicGameElement").getProperty("xPos").setValue(new Double(2));
-		go1.getBehavior("BasicGameElement").getProperty("yPos").setValue(new Double(7));
-		go1.getBehavior("BasicGameElement").getProperty("elementName").setValue("this is my name");
-
-//		GameObject go2 = new GameObject(bf.makeBehavior("BasicGameElement"));
-//		go2.addBehavior(bf.makeBehavior("Movable"));
-		System.out.println(go1.getBehaviors());
-//		System.out.println(go2.getBehaviors());
-
-		gameScene.addObject(go1);
+//		GameScene gameScene = new GameScene("Level 1");
+//		BehaviorFactory bf = new BehaviorFactory();
+//		GameObject go1 = new GameObject(bf.makeBehavior("BasicGameElement"));
+//		go1.getBehavior("BasicGameElement").getProperty("xPos").setValue(new Double(2));
+//		go1.getBehavior("BasicGameElement").getProperty("yPos").setValue(new Double(7));
+//		go1.getBehavior("BasicGameElement").getProperty("elementName").setValue("this is my name");
+//
+////		GameObject go2 = new GameObject(bf.makeBehavior("BasicGameElement"));
+////		go2.addBehavior(bf.makeBehavior("Movable"));
+//		System.out.println(go1.getBehaviors());
+////		System.out.println(go2.getBehaviors());
+//
+//		gameScene.addObject(go1);
+//		
+//		ArrayList<GameScene> gameList = new ArrayList<>();
+//		gameList.add(gameScene);
+//		
+//		GameInitializer gameInitializer = new GameInitializer("gouttham");
+//		GameSaver gameSaver = new GameSaver("gouttham");
+//		gameSaver.addDescription("HELLO", "THIS IS A TEST GAME.", null);
+//		gameSaver.gameAuthorToXML(gameList);
+//		
+//		GameLoader gameLoader = new GameLoader("gouttham");
+//		GameScene pls = gameLoader.getGameScenes().get(0);
+//		System.out.println(pls.getMyElements().get(0).getBehaviors());
 		
-		ArrayList<GameScene> gameList = new ArrayList<>();
-		gameList.add(gameScene);
-		
-		GameInitializer gameInitializer = new GameInitializer("gouttham");
-		GameSaver gameSaver = new GameSaver("gouttham");
-		gameSaver.addDescription("HELLO", "THIS IS A TEST GAME.", null);
-		gameSaver.gameAuthorToXML(gameList);
-		
-		GameLoader gameLoader = new GameLoader("gouttham");
-		GameScene pls = gameLoader.getGameScenes().get(0);
-		System.out.println(pls.getMyElements().get(0).getBehaviors());
+		ModelGameState model = new ModelGameState();
+		GameInitializer i = new GameInitializer("test");
+		GameSaver saver = new GameSaver("test");
+		saver.saveGameState(model.getState());
 		
 		
 		
--- src/authoring/display/GameViewWindow.java
+++ src/authoring/display/GameViewWindow.java
@@ -3,8 +3,12 @@ package authoring.display;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import authoring.Behavior;
 import authoring.Game;
 import authoring.GameObject;
+import authoring.Property;
+import data.ImageManager;
+import engine.behaviors.MandatoryBehavior;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -16,18 +20,40 @@ public class GameViewWindow extends AuthoringUIComponent{
 	StackPane stackPane;
 	TilePane tilePane;
 	Pane pane;
+	ImageManager imageManager;
 	
 	public GameViewWindow(ResourceBundle resources, Game game, Node root){
 		super(resources, game, root);
-		
+		stackPane = new StackPane();
+		tilePane = new TilePane();
+		pane = new Pane();
+		setGameObjectList(game);
+		addObjectsToPane();
 	}
 
 	private void setGameObjectList(Game game){
-		game.getSceneManager().getCurrentScene();
+		gameObjects = game.getSceneManager().getCurrentScene().getMyObjects();
 	}
 	
 	
 	private void addObjectsToPane() {
+		for (GameObject object: gameObjects) {
+			Behavior mandatoryBehavior = object.getBehavior("MandatoryBehavior");
+			Property xPositionProperty = mandatoryBehavior.getProperty("xPos");
+			Property yPositionProperty = mandatoryBehavior.getProperty("yPos");
+			Property imagePathProperty = mandatoryBehavior.getProperty("imagePath");
+			Double xPosition = (Double) xPositionProperty.getValue();
+			Double yPosition = (Double) yPositionProperty.getValue();
+			String imagePath = (String) imagePathProperty.getValue();
+			placeObject(xPosition, yPosition, imagePath);
+		}
+	}
+	
+	private void placeObject(Double x, Double y, String imagePath) {
+		
+	}
+	
+	private void makeBackgroundPane() {
 		
 	}
 	

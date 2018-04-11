06694829fe7128ab108f64bce4a4dd44fb3ddd44
Edward Zhuang <edward.zhuang@duke.edu>
2018-04-10 22:22:06 -0400

    working on gameviewwindow

--- src/authoring/display/GameViewWindow.java
+++ src/authoring/display/GameViewWindow.java
@@ -1,16 +1,36 @@
 package authoring.display;
 
+import java.util.List;
 import java.util.ResourceBundle;
 
 import authoring.Game;
+import authoring.GameObject;
 import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 
 public class GameViewWindow extends AuthoringUIComponent{
 
+	List<GameObject> gameObjects;
+	StackPane stackPane;
+	TilePane tilePane;
+	Pane pane;
+	
 	public GameViewWindow(ResourceBundle resources, Game game, Node root){
 		super(resources, game, root);
+		
 	}
 
+	private void setGameObjectList(Game game){
+		game.getSceneManager().getCurrentScene();
+	}
+	
+	
+	private void addObjectsToPane() {
+		
+	}
+	
 	public Node asPane() {
 		// TODO Auto-generated method stub
 		return null;

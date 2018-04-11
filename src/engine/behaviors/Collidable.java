8ffc09c7d0a7dc15d084d9d1d7a7d8f2ca312a80
cgouttham <cgouttham@gmail.com>
2018-04-10 23:30:23 -0400

    add changes

--- src/engine/behaviors/Collidable.java
+++ src/engine/behaviors/Collidable.java
@@ -1,20 +1,21 @@
 package engine.behaviors;
+
 import java.util.Arrays;
 import java.util.List;
+
 import engine.GameElement;
 
-/**
- * @author Gouttham 
- * The Game Elements which acquires this property have the ability to inherit event responses that allow it to respond to gravity. 
- *
- */
-public class Gravity extends Behavior{
+public class Collidable extends Behavior{
 	public static final double GRAVITATIONAL_FORCE = -9.8;
 	private Movable b;
 	
-	public Gravity(GameElement ge) {
+	public Collidable(GameElement ge) {
 		super(ge);
 		b = (Movable) ge.getBehavior(Movable.class);
+	}
+	
+	public Collidable(GameElement ge) {
+		this(ge, 5.0, Arrays.asList(0.0, 1.0));
 	}
 	
 	public void experienceGravity(Double time) {

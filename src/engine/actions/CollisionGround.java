859650bc382222cd91e0b30965778d8ab9cfc16b
cgouttham <cgouttham@gmail.com>
2018-04-11 01:51:51 -0400

    Added collisions and booty method to kill

--- src/engine/actions/CollisionGround.java
+++ src/engine/actions/CollisionGround.java
@@ -7,18 +7,22 @@ import engine.behaviors.Movable;
 import engine.events.elementevents.CollisionEvent;
 import engine.events.elementevents.ElementEvent;
 
-public class CollisionKillable implements Action {
+public class CollisionGround implements Action {
 
 	@Override
 	public void act(ElementEvent event, GameElement element) {
 		CollisionEvent ce = (CollisionEvent) event;
-		Killable b = (Killable) element.getBehavior(Killable.class);
 		Movable b2 = (Movable) element.getBehavior(Movable.class);
 		MandatoryBehavior b3 = (MandatoryBehavior) element.getBehavior(MandatoryBehavior.class);
-		b.loseLife();
-		b2.setXVelocity(0.0);
+		if (b2.getXVelocity()>0) {
+			b2.setXVelocity(b2.getXVelocity()-20);
+		}
+		if (b2.getXVelocity()<0) {
+			b2.setXVelocity(b2.getXVelocity()+20);
+		}
 		b2.setYVelocity(20.0);
-		b3.setPosition(-100.0, -100.0);
+		b3.setPosition(b3.getX(), b3.getY()-1);
+		
 	}
 
 }

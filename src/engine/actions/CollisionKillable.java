c869bb75dc8aaf96ba24ccdfbb053afe3a650308
Trishul Nagenalli <tn74@duke.edu>
2018-04-17 00:43:07 -0400

    Complete for night. awaiting visualization of element death

--- src/engine/actions/CollisionKillable.java
+++ src/engine/actions/CollisionKillable.java
@@ -11,15 +11,8 @@ public class CollisionKillable implements Action {
 
 	@Override
 	public void act(ElementEvent event, GameElement element) {
-		CollisionEvent ce = (CollisionEvent) event;
 		Killable b = (Killable) element.getBehavior(Killable.class);
 		b.loseLife();
-//		Movable b2 = (Movable) element.getBehavior(Movable.class);
-//		MandatoryBehavior b3 = (MandatoryBehavior) element.getBehavior(MandatoryBehavior.class);
-//		b.loseLife();
-//		b2.setXVelocity(0.0);
-//		b2.setYVelocity(20.0);
-//		b3.setPosition(-100.0, -100.0);
 	}
 
 }

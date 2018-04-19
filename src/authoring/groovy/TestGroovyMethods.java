53ae7def7b45b2e10065b171a02f1366072a3e51
Summer Smith <sas118@duke.edu>
2018-04-19 14:20:08 -0400

    debuging groovy test files

--- src/authoring/groovy/TestGroovyMethods.java
+++ src/authoring/groovy/TestGroovyMethods.java
@@ -3,6 +3,7 @@ package authoring.groovy;
 public class TestGroovyMethods {
 
     public static void main(String[] args){
+    		System.out.println("hey");
 	    GroovyCommandFactory factory = new GroovyCommandFactory();
 	    System.out.println(factory.getBehaviorFields());
 	    System.out.println(factory.getBehaviorMethods());

8ba351efe499bbc815850fe9b026de880773d9ab
Summer Smith <sas118@duke.edu>
2018-04-29 16:31:16 -0400

    remove print statement

--- src/authoring/displayrefactored/popups/behaviorspopup/BehaviorPanel.java
+++ src/authoring/displayrefactored/popups/behaviorspopup/BehaviorPanel.java
@@ -70,7 +70,6 @@ public class BehaviorPanel {
 	private ComboBox<AuthBehavior> makeBehaviorDropdown() {
 		ComboBox<AuthBehavior> behaviorDropdown = new ComboBox<>();
 		behaviorDropdown.getItems().addAll(notMyBehaviors);
-		System.out.println("dropdown content (BehaviorPanel line 73): "+ behaviorDropdown.getItems());
 		return behaviorDropdown;
 	}
 	

e30d6c2783d7a43bb71093c2206170006d2508a4
Trishul Nagenalli <tn74@duke.edu>
2018-04-29 00:01:19 -0400

    Eddied loaded the saved scene into Authoring

--- src/data/test/GameSceneSerializeTest.java
+++ src/data/test/GameSceneSerializeTest.java
@@ -6,6 +6,7 @@ import com.thoughtworks.xstream.XStream;
 
 import authoring.GameObject;
 import authoring.GameScene;
+import authoring.GameSceneSerializable;
 import engine.actions.GroovyAction;
 import engine.authouringconversion.Converter2;
 import engine.authouringconversion.Printer;
@@ -22,18 +23,19 @@ public class GameSceneSerializeTest {
 		ModelGamePart1 mp1 = new ModelGamePart1();
 		XStream xstream = new XStream();
 		GameScene modelGamePart1Scene = converter.gamePart2GameScene(new ModelGamePart1().getGamePart());
+		GameSceneSerializable serializable = new GameSceneSerializable(modelGamePart1Scene);
 		String levelGameDestination = "./data/gamedata/games/enginetestmario/scenes/testscene";
 		
 		Printer printer = new Printer();
 		printer.printScene(modelGamePart1Scene);
 		
-		GameObject block = converter.gameElement2GameObject(mp1.getMario());
+//		GameObject block = converter.gameElement2GameObject(mp1.getMario());
 //		String inputEvent = xstream.toXML(new KeyInputEvent(KeyCode.P));
 //		String blockString = xstream.toXML(block);
-		String respString = xstream.toXML(new GroovyAction("EXECUTIONER"));
-//		String xmlString = xstream.toXML(modelGamePart1Scene);
+//		String respString = xstream.toXML(new GroovyAction("EXECUTIONER"));
+		String xmlString = xstream.toXML(serializable);
 		java.io.FileWriter fw = new java.io.FileWriter(levelGameDestination + ".xml");
-//        fw.write(xmlString);
+        fw.write(xmlString);
         fw.close();
 	}
 }

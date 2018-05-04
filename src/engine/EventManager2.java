d6625072f8a6a8375a9b7b9f8e8c6c30889e80d4
Martin <mm617@duke.edu>
2018-05-03 22:01:38 -0400

    Javadoc

--- src/engine/EventManager2.java
+++ src/engine/EventManager2.java
@@ -7,34 +7,47 @@ import engine.collision.CollisionManager;
 import engine.events.elementevents.ElementEvent;
 import engine.events.gameevents.GameEvent;
 
+/**
+ * @author Martin
+ * 
+ * Construct a new EventManager, used to receive, process, and handle GameEvents
+ * and ElementEvents
+ *
+ */
 public class EventManager2 {
 	
 	private GameState gameSate;
 	private CollisionManager collisionManager;
 	
+	/**
+	 * Construct an EventManager with a GameState
+	 * 
+	 * @param state GameState of the current game
+	 */
 	public EventManager2 (GameState state) {
 		gameSate = state;
 		collisionManager = new CollisionManager();
 	}
 	
+	/**
+	 * Send an ElementEvent to the game elements of the current game part to be processed,
+	 * and store the returned GameEvents.
+	 * @param ee
+	 */
 	public void processElementEvent(ElementEvent ee) {
-	
 		List<GameEvent> gameEvents = new ArrayList<>();
 		for (GameElement ge: gameSate.getCurrentGamePart().getElements()) {
 			gameEvents.addAll(ge.processEvent(ee));
 		}
 		gameEvents.addAll(collisionManager.handleCollisions(gameSate.getCurrentGamePart()));
 		gameEvents.stream().forEach(event -> processGameEvent(event));
-		
 	}
 	
+	/**
+	 * Execute a game event on the current GameState.
+	 * @param gameEvent GameEvent to be processed
+	 */
 	private void processGameEvent(GameEvent gameEvent) {
-		//System.out.println("GameEvent processed");
 		gameEvent.execute(gameSate);
-	}
-
-	
-	public GamePart getCurrentPart() {
-		return gameSate.getCurrentGamePart();
 	}
 }

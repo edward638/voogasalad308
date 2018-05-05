7e324a39322c48ed9c93aaddb543f3aa91da8650
Calvin Ma <calvin.m.ma@duke.edu>
2018-05-04 21:16:34 -0400

    missing merge from morning - just updating magic values

--- src/gamePlayer/buttons/GameSelector.java
+++ src/gamePlayer/buttons/GameSelector.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.nio.file.Paths;
 
 import data.GameDescriptionProvider;
-
+import engine.exceptions.ErrorBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -36,14 +36,31 @@ public class GameSelector extends ScrollPane {
 	private static final String loadGameString = "Load Game";
 	private static final String font = "Times New Roman";
 	private static final Color color = Color.WHITE;
+	private static final int LAYOUT_OFFSET = 30;
+	private static final int SELECTOR_WIDTH = 900;
+	private static final int SELECTOR_HEIGHT = 590;
+	private static final int SELECTORBOX_SPACING = 10;
+	private static final double IMAGE_OPACITY = 0.4;
+	private static final int BUTTON_X = 750;
+	private static final int BUTTON_Y = 90;
+	private static final int GAMEIMAGE_OFFSET = 20;
+	private static final int GAMEIMAGE_SIZE = 160;
+	private static final int DESCRIPTION_X = 230;
+	private static final int DESCRIPTION_Y = 90;
+	private static final String FANCY_TEXT = " -fx-font: 50px Helvetica;\\r\\n\"\r\n"
+			+ "				+ \"    -fx-fill: linear-gradient(from 0% 0% to 100% 200%, repeat, aqua 0%, red 50%);\\r\\n\"\r\n"
+			+ "				+ \"    -fx-stroke: black;\\r\\n\" + \"    -fx-stroke-width: 1;";
+	private static final int NAMETEXT_X = 225;
+	private static final int NAMETEXT_Y = 60;
 
 	public GameSelector(ButtonData buttonData, boolean isNew) {
 		this.buttonData = buttonData;
 		buttonData.setEngineRunning(false);
 		buttonData.pauseGame();
-		this.setLayoutX(30);
-		this.setLayoutY(30);
-		this.setPrefSize(900, 590);
+
+		this.setLayoutX(LAYOUT_OFFSET);
+		this.setLayoutY(LAYOUT_OFFSET);
+		this.setPrefSize(SELECTOR_WIDTH, SELECTOR_HEIGHT);
 		this.setHbarPolicy(ScrollBarPolicy.NEVER);
 		this.isNew = isNew;
 		if (isNew) {
@@ -71,7 +88,7 @@ public class GameSelector extends ScrollPane {
 				Pane gameDescriptionPane = setupNewGamePane(gameName, gameString, gameDescription, gameImage);
 				gameSelectorBox.getChildren().add(gameDescriptionPane);
 			} catch (Exception e) {
-				System.out.println("Failed to load in game from folder " + gameName);
+				// Error will never happen because the selector only shows legal files
 				continue;
 			}
 		}
@@ -80,7 +97,8 @@ public class GameSelector extends ScrollPane {
 
 	private void setUpGameSelectorBox() {
 		gameSelectorBox = new VBox();
-		gameSelectorBox.setSpacing(10);
+
+		gameSelectorBox.setSpacing(SELECTORBOX_SPACING);
 		gameSelectorBox.setStyle("-fx-background-color: transparent;");
 
 		gameSelectorBox.setMinWidth(boxWidth);
@@ -91,7 +109,8 @@ public class GameSelector extends ScrollPane {
 		Pane pane = new Pane();
 
 		ImageView paneImage = new ImageView(new Image("file:background_image.jpg"));
-		paneImage.setOpacity(0.4);
+
+		paneImage.setOpacity(IMAGE_OPACITY);
 		paneImage.setFitWidth(boxWidth);
 		paneImage.setFitHeight(boxHeight);
 		pane.getChildren().add(paneImage);
@@ -114,8 +133,9 @@ public class GameSelector extends ScrollPane {
 
 	private Button makePlayButton(String gameName) {
 		Button playButton = new Button(playOrLoadGame);
-		playButton.setLayoutX(750);
-		playButton.setLayoutY(90);
+
+		playButton.setLayoutX(BUTTON_X);
+		playButton.setLayoutY(BUTTON_Y);
 		playButton.setOnAction((event) -> {
 			buttonData.removeFromRoot(gameSelectorBox);
 			buttonData.removeFromRoot(this);
@@ -126,8 +146,9 @@ public class GameSelector extends ScrollPane {
 
 	private Text setUpGameDescriptionText(String gameDescription) {
 		Text descriptionText = new Text(gameDescription);
-		descriptionText.setX(230);
-		descriptionText.setY(90);
+
+		descriptionText.setX(DESCRIPTION_X);
+		descriptionText.setY(DESCRIPTION_Y);
 		descriptionText.setFont(new Font(font, 20));
 		descriptionText.setFill(color);
 		return descriptionText;
@@ -136,20 +157,18 @@ public class GameSelector extends ScrollPane {
 	private ImageView setUpGameImage(Image gameImage) {
 		ImageView imageView = new ImageView();
 		imageView.setImage(gameImage);
-		imageView.setX(20);
-		imageView.setY(20);
-		imageView.setFitHeight(160);
-		imageView.setFitWidth(160);
+		imageView.setX(GAMEIMAGE_OFFSET);
+		imageView.setY(GAMEIMAGE_OFFSET);
+		imageView.setFitHeight(GAMEIMAGE_SIZE);
+		imageView.setFitWidth(GAMEIMAGE_SIZE);
 		return imageView;
 	}
 
 	private Text setUpGameText(String gameString) {
 		Text nameText = new Text(gameString);
-		nameText.setX(225);
-		nameText.setY(60);
-		nameText.setStyle(" -fx-font: 50px Helvetica;\r\n"
-				+ "    -fx-fill: linear-gradient(from 0% 0% to 100% 200%, repeat, aqua 0%, red 50%);\r\n"
-				+ "    -fx-stroke: black;\r\n" + "    -fx-stroke-width: 1;");
+		nameText.setX(NAMETEXT_X);
+		nameText.setY(NAMETEXT_Y);
+		nameText.setStyle(FANCY_TEXT);
 		return nameText;
 	}
 }

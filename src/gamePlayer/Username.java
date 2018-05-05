e0cdf52581a07c2902b479a2901993d3b7a225e6
Calvin Ma <calvin.m.ma@duke.edu>
2018-05-04 21:15:15 -0400

    missing merge from this morning - just updating magic values

--- src/gamePlayer/Username.java
+++ src/gamePlayer/Username.java
@@ -8,7 +8,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 /**
- * class for managing everything username related including setting, changing, adding high scores
+ * class for managing everything username related including setting, changing,
+ * adding high scores
+ * 
  * @author jeffreyli, calvinma
  *
  */
@@ -17,6 +19,10 @@ public class Username {
 	private static final String FONT = "Din Alternate";
 	private Label nameText;
 	private static final int fontSize = 20;
+	private static final String DIALOGTITLE = "Username";
+	private static final String DIALOGTEXT = "Welcome to VoogaSalad!";
+	private static final String DIALOGCONTENT = "Please enter your username: ";
+	private static final String USERTEXT = "User: ";
 
 	public Username() {
 		setupUserNameInput();
@@ -25,32 +31,33 @@ public class Username {
 
 	private void setupUserNameInput() {
 		TextInputDialog dialog = new TextInputDialog();
-		dialog.setTitle("Username");
-		dialog.setHeaderText("Welcome to VoogaSalad!");
-		dialog.setContentText("Please enter your username:");
+		dialog.setTitle(DIALOGTITLE);
+		dialog.setHeaderText(DIALOGTEXT);
+		dialog.setContentText(DIALOGCONTENT);
 		Optional<String> result = dialog.showAndWait();
 		result.ifPresent(name -> userName = result.get());
 	}
 
 	private void setupUserNameText() {
-		nameText = new Label("User: " + userName);
+		nameText = new Label(USERTEXT + userName);
 		nameText.setLayoutX(970);
 		nameText.setLayoutY(10);
-		nameText.setFont(Font.font(FONT, 20));
+		nameText.setFont(Font.font(FONT, fontSize));
 		nameText.setTextFill(Color.WHITE);
 	}
 
 	public Label getNameText() {
 		return nameText;
 	}
-	
+
 	/**
 	 * changes the username and updates the label to have the new name
+	 * 
 	 * @param newName
 	 */
 	public void changeName(String newName) {
 		userName = newName;
-		nameText.setText("User: " + userName);
+		nameText.setText(USERTEXT + userName);
 	}
 
 	public String getName() {
